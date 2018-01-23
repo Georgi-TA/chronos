326d0983b0c7dfc076bb362b8b492d20283c501a
Georgi Chakarov <george.chakarov@primeholding.com>
2018-01-23 11:19:43 +0200

    Fixed requesting permissions

--- app/src/main/java/com/touchawesome/chronos/CallLogAdapter.kt
+++ app/src/main/java/com/touchawesome/chronos/CallLogAdapter.kt
@@ -11,9 +11,9 @@ import kotlinx.android.synthetic.main.li_call_log.view.*
 /**
  * Created by scelus on 1/22/18
  */
-class CallLogAdapter(log: ArrayList<CallLogEntry>) : RecyclerView.Adapter<CallLogAdapter.CallLogVH> () {
+class CallLogAdapter() : RecyclerView.Adapter<CallLogAdapter.CallLogVH> () {
 
-    val data: ArrayList<CallLogEntry> = log;
+    val data: ArrayList<CallLogEntry> = ArrayList();
 
     override fun getItemCount(): Int {
        return data.size
@@ -34,12 +34,19 @@ class CallLogAdapter(log: ArrayList<CallLogEntry>) : RecyclerView.Adapter<CallLo
             itemView.call_log_name.text = item.name
             itemView.call_log_number.text = item.number
 
-            Picasso.with(itemView.context)
-                   .load(item.photo).resize(200, 200)
+            if (item.photo != null)
+                Picasso.with(itemView.context)
+                    .load(item.photo)
+                    .resize(64, 64)
                     .centerCrop()
                     .into(itemView.call_log_image)
 
-            itemView.call_log_date.text = DateUtils.formatElapsedTime(System.currentTimeMillis() / 1000 - item.timestamp)
+            itemView.call_log_date.text = DateUtils.getRelativeTimeSpanString(item.timestamp, System.currentTimeMillis() / 1000, DateUtils.DAY_IN_MILLIS)
         }
+    }
+
+    fun setData(data: ArrayList<CallLogEntry>) {
+        this.data.clear()
+        this.data.addAll(data)
     }
 }
--- app/src/main/java/com/touchawesome/chronos/MainActivity.kt
+++ app/src/main/java/com/touchawesome/chronos/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.touchawesome.chronos
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
@@ -12,11 +13,9 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
-
 class MainActivity : AppCompatActivity() {
     private val REQUEST_READ_CALL_LOG = 15151
-    private val data = ArrayList<CallLogEntry>()
-    private val adapter = CallLogAdapter(data)
+    private val adapter = CallLogAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +25,10 @@ class MainActivity : AppCompatActivity() {
         call_log_list.adapter = adapter
         call_log_list.setHasFixedSize(true)
 
-        val permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG)
-        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+        if (!checkPermissions()) {
             // request the permission.
             ActivityCompat.requestPermissions(this,
-                    arrayOf(Manifest.permission.READ_CONTACTS),
+                    arrayOf(Manifest.permission.READ_CALL_LOG, Manifest.permission.READ_CONTACTS),
                     REQUEST_READ_CALL_LOG)
         }
         else {
@@ -38,25 +36,32 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun loadCallLog() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-            data.clear()
+    private fun checkPermissions() : Boolean {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED &&
+               ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) == PackageManager.PERMISSION_GRANTED
+    }
 
-            val c = contentResolver.query(CallLog.Calls.CONTENT_URI, arrayOf(CallLog.Calls.CACHED_NAME, CallLog.Calls.NUMBER, CallLog.Calls.DATE), null, null, null)
+    @SuppressLint("MissingPermission")
+    private fun loadCallLog() {
+        if (checkPermissions()) {
+
+            val data = ArrayList<CallLogEntry>()
+            val c = contentResolver.query(CallLog.Calls.CONTENT_URI, arrayOf(CallLog.Calls.CACHED_NAME, CallLog.Calls.NUMBER, CallLog.Calls.DATE), null, null, CallLog.Calls.DATE + " DESC")
+
             if (c.moveToFirst()) {
                 for (i in 0..100) {
                     val item = CallLogEntry(c.getString(c.getColumnIndex(CallLog.Calls.CACHED_NAME)),
-                            c.getString(c.getColumnIndex(CallLog.Calls.NUMBER)),
-                            null,
-                            c.getLong(c.getColumnIndex(CallLog.Calls.DATE)))
+                                            c.getString(c.getColumnIndex(CallLog.Calls.NUMBER)),
+                                            null,
+                                            c.getLong(c.getColumnIndex(CallLog.Calls.DATE)))
 
                     // Query for Photo if possible
                     val contactUri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(item.number))
 
                     // querying contact data store
-                    val phones = contentResolver.query(contactUri, arrayOf(ContactsContract.PhoneLookup.PHOTO_URI), null, null, null)
-                    item.photo = Uri.parse(phones.getString(phones.getColumnIndex(ContactsContract.PhoneLookup.PHOTO_URI)))
-                    phones.close()
+//                    val phones = contentResolver.query(contactUri, arrayOf(ContactsContract.PhoneLookup.PHOTO_URI), null, null, null)
+//                    item.photo = Uri.parse(phones.getString(phones.getColumnIndex(ContactsContract.PhoneLookup.PHOTO_URI)))
+//                    phones.close()
 
                     data.add(item)
 
@@ -66,7 +71,9 @@ class MainActivity : AppCompatActivity() {
                         break
                 }
             }
+
             c.close()
+            adapter.setData(data)
             adapter.notifyDataSetChanged()
         }
     }
@@ -74,8 +81,13 @@ class MainActivity : AppCompatActivity() {
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         when (requestCode) {
             REQUEST_READ_CALL_LOG -> {
+                var granted = true;
+                for (permissionGrant in grantResults) {
+                    granted  = granted && (permissionGrant == PackageManager.PERMISSION_GRANTED)
+                }
+
                 // If request is cancelled, the result arrays are empty.
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                if (granted) {
                     loadCallLog()
                 }
                 return
